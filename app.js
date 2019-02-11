571b223ffa6376bcceb96f31582bd8cce9da715b
IBUSINESS-L025\Rajasoundarya <rajasoundarya.m@ibusinesssoftware.in>
2019-02-11 15:03:11 +0530

    mysql connection and data retreival

--- app.js
+++ app.js
@@ -3,17 +3,46 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+var mysql      = require('mysql');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const nav = [
+  { link: '/authors', category: 'authors' },
+  { link: '/books', category: 'books' }
+];
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use(
+  '/css',
+  express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css'))
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'))
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, 'node_modules/jquery/dist'))
+);
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+
+app.use('/books', bookRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  //res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index', {
+    nav: [
+      { link: '/authors', category: 'authors' },
+      { link: '/books', category: 'books' }
+    ],
+    title: 'My title should be  here!'
+  });
 });
 
 app.listen(port, () => {
