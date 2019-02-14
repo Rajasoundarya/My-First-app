940aba29a9bb7631d940633a27d40df35a6c32e6
IBUSINESS-L025\Rajasoundarya <rajasoundarya.m@ibusinesssoftware.in>
2019-02-14 15:40:49 +0530

    local strategy

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+require('./strategies/local.strategy');
 
 function passportConfig(app){
   app.use(passport.initialize());
@@ -11,6 +12,6 @@ function passportConfig(app){
     done(null, user);
   });
 
-  require('./strategies/local.strategy');
+
 }
 module.exports = passportConfig;
